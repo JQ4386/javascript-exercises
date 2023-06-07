2a2a245142cf41b3ec1dc13c731abc4ce0fe1197
JQ4386 <jingqi_qu@berkeley.edu>
2023-06-08 04:25:17 +1000

    Exercise 1-7 Done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(str, repetitions) {
+    if (repetitions < 0) {
+        return "ERROR"
+    } 
+    return str.repeat(repetitions);
 };
 
 // Do not edit below this line
